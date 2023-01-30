cbb0d2f40c2ed56941bc8df26713e9a938ccae35
Jimmy Fang <fangjian0423@gmail.com>
2023-01-30 21:01:34 +0800

    enable log analytics

--- infra/modules/applicationinsights/applicationinsights.tf
+++ infra/modules/applicationinsights/applicationinsights.tf
@@ -25,7 +25,7 @@ resource "azurerm_application_insights" "applicationinsights" {
   location            = var.location
   resource_group_name = var.rg_name
   application_type    = "web"
-#  workspace_id        = var.workspace_id
+  workspace_id        = var.workspace_id
   tags                = var.tags
 }
 
