101b16238442d65c609e3f39ff2eee5cead4f387
Jimmy Fang <fangjian0423@gmail.com>
2023-01-31 22:02:43 +0800

    adding powshell to support Windows

--- infra/modules/appservicejava/appservicejava.tf
+++ infra/modules/appservicejava/appservicejava.tf
@@ -62,13 +62,25 @@ resource "azurerm_linux_web_app" "web" {
   }
 }
 
+locals {
+  test = pathexpand("~/${path.module}/main.tf")
+  command_map = substr(local.test,0,1) == "/"? {
+    command = "./scripts/pg-create-aad-role.sh ${var.pg_server_fqdn} ${azurerm_linux_web_app.web.identity.0.principal_id} ${var.pg_database_name} ${var.pg_aad_admin_user} ${var.pg_custom_role_name_with_aad_identity}",
+    interpreter = "bash"
+  }:{
+    command = "./scripts/pg-create-aad-role.ps1 ${var.pg_server_fqdn} ${azurerm_linux_web_app.web.identity.0.principal_id} ${var.pg_database_name} ${var.pg_aad_admin_user} ${var.pg_custom_role_name_with_aad_identity}",
+    interpreter = "powershell"
+  }
+}
+
 resource "azurecaf_name" "app_umi" {
   resource_type       = "azurerm_user_assigned_identity"
   name                = "pqsl-script"
 
   provisioner "local-exec" {
-    command     = "./scripts/pg-create-aad-role.sh ${var.pg_server_fqdn} ${azurerm_linux_web_app.web.identity.0.principal_id} ${var.pg_database_name} ${var.pg_aad_admin_user} ${var.pg_custom_role_name_with_aad_identity}"
+    command = local.command_map.command
+    interpreter = [local.command_map.interpreter]
     working_dir = path.module
-    when        = create
+    when = create
   }
 }
