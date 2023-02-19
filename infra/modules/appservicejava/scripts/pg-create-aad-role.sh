6f4a3e9e981ad4cb273c88bda404bd321918ee02
Jimmy Fang <fangjian0423@gmail.com>
2023-02-19 15:55:03 +0800

    optimize create aad role script

--- infra/modules/appservicejava/scripts/pg-create-aad-role.sh
+++ infra/modules/appservicejava/scripts/pg-create-aad-role.sh
@@ -6,7 +6,11 @@ CUSTOM_ROLE=$5
 
 sleep 60;
 
-echo "PostgreSQL Server ${PSQL_SERVER_NAME} creating AD role in database ${DATABASE_NAME} on ${DATABASE_FQDN}..."
+echo "PostgreSQL Server creating AD role in database ${DATABASE_NAME} on ${DATABASE_FQDN}..."
+
+if [ "${ARM_CLIENT_ID}" != "" ] && [ "${ARM_CLIENT_SECRET}" != "" ] && [ "${ARM_TENANT_ID}" != "" ];then
+  az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
+fi
 
 RDBMS_ACCESS_TOKEN=$(az account get-access-token --resource-type oss-rdbms --output tsv --query accessToken)
 
